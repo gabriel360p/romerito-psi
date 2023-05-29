3aa8ae1258f0b216e58842f3aeaf77d3844d91ff
Gabriel Costa da Silva <20221101110085@ifrn.local>
2023-05-29 10:01:32 -0300

    projeto 10

--- projeto10/index.php
+++ projeto10/index.php
@@ -1,11 +1,46 @@
 <?php
 
+session_start
+
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+if($method=='POST'){
 
-switch($uri){
+    switch($uri){
+        case '/store':
+            include __DIR__.'/store';
+            break;
+
+        case '/logout':
+
+            break;
+
+    }
+
+}else if($method=='GET'){
     
+    switch($uri){
+        case '/':
+            include __DIR__.'/views/home.php';
+            break;
+
+        case '/newsession':
+            include __DIR__.'/views/home.php';
+            break;
+
+        case '/products':
+            include __DIR__.'/views/products/home.php';
+            break;
+
+        case '/cart':
+            include __DIR__.'/views/cart/home.php';
+        break;
+
+
+    }
+
 }
+
 
 ?>
---
+++ projeto10/views/home.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Welcome</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <header>
+    
+    <div class="container text-center">
+        <h1 class="">Bem Vindo</h1>
+        <h5 class="border-bottom">Simple Market</h5>
+    </div>
+
+  </header>
+
+  <main>
+
+<div class="container text-center mt-4">
+    <div class="btn-group">
+        <a class="btn btn-outline-primary">Iniciar Sessão</a>
+        <a class="btn btn-outline-primary">Ver Produtos</a>
+        <a class="btn btn-outline-primary">Ver Carrinho</a>
+    </div>
+</div>
+
+</main>
+
+  <footer>
+    <!-- place footer here -->
+  </footer>
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
